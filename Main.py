c7a944f7889cfd11537b867ae0c2fcc21b59ad02
Paola <paolaleonardiruvinski@gmail.com>
2026-04-15 22:57:51 -0300

    Delete Main.py
    Deletando uma cópia vazia do main

--- Main.py
+++
@@ -1,7 +0,0 @@
-# --------------- Menú ----------------- ---------
-
-# --------------- Helena: Humano x Humano --------
-
-# --------------- Juliana: Computador x Humano ---
-
-# --------------- Paola: Computador x Computador -
